1b2c25d79308ea9a980c47accfe5c962dd1ec289
Telesphore Uwabera <t.uwabera@alustudent.com>
2025-07-21 21:48:02 +0200

    Deployed web app

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -12,7 +12,9 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(cors());
+app.use(cors({
+  origin: 'https://africanfintechindex.netlify.app',
+}));
 app.use(express.json());
 
 app.use('/api/auth', authRoutes);
